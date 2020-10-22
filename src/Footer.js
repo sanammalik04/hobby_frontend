ecb951aa7f3edc000aa9e01ac9244cc5ee461d4c
github username <github email address>
2020-10-21 23:50:59 -0400

    finalized all styling

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { Container } from "semantic-ui-react";
+// import React from "react";
+// import { Container } from "semantic-ui-react";
 
-const Footer = () => (
-    <Container className='foot'>
-  <div className="footer">
-    <h1>Good Day Crafting!</h1>
-  </div>
-  </Container>
-);
+// const Footer = () => (
+//     <Container className='foot'>
+//   <div className="footer">
+//     <h1>Good Day Crafting!</h1>
+//   </div>
+//   </Container>
+// );
 
-export default Footer;
+// export default Footer;
